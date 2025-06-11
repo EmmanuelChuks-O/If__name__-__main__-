ca7b904a483db99187ae67412f6632c6e0561be1
Emmanuel Chuks Okoh <okoh.ce@gmail.com>
2025-06-10 20:28:43 -0400

    Add files via upload

---
+++ Script2.py
@@ -0,0 +1,13 @@
+from Script1 import *
+
+def favorite_drink(drink):
+    print(f"Your favorite drink is {drink}")
+
+def main():
+    print("This is script2")
+    favourite_food("Eggs")
+    favorite_drink("Apple juice")
+    print("Goodbye!")
+
+if __name__ == '__main__':
+    main()
